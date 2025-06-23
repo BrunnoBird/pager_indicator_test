ea894d65b62058747bc4c9fa05018bbf52d46769
brunno <bird.tec.gonzalez@gmail.com>
2025-06-23 12:04:57 -0300

    Align `PagerIndicatorContent` and remove background
    The `PagerIndicatorContent` in the sample app is now centered horizontally and vertically.
    The light gray background from the `PagerIndicator` in the sample app has been removed.
    An unnecessary closing curly brace was removed from `PagerIndicator.kt`.

--- app/src/main/java/com/tek/pager_indicator/MainActivity.kt
+++ app/src/main/java/com/tek/pager_indicator/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -13,6 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -24,10 +26,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Surface(modifier = Modifier.fillMaxSize()) {
-                Column(Modifier.fillMaxSize()) {
-                    Column(modifier = Modifier.weight(1f)) {
-                        PagerIndicatorContent()
-                    }
+                Column(
+                    Modifier
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    PagerIndicatorContent()
                 }
             }
         }
@@ -40,9 +45,7 @@ fun PagerIndicatorContent() {
     val pageCount = 11
 
     PagerIndicator(
-        modifier = Modifier
-            .height(50.dp)
-            .background(Color.LightGray),
+        modifier = Modifier,
         pageCount = pageCount,
         currentIndex = currentIndex,
         onIndexChange = { currentIndex = it }
